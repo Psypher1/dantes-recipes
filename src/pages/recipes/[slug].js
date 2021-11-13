4862dabccc3d7eabd78228045af6fe2fc17d0188
Dante <jamesmidzi@gmail.com>
2021-11-13 20:35:55 +0200

    did initial code for dynamic page

--- src/pages/recipes/[slug].js
+++ src/pages/recipes/[slug].js
@@ -1,13 +1,47 @@
+import client from "../../utils/contentfulClient";
+
 export async function getStaticPaths() {
+  const res = await client.getEntries({
+    content_type: "recipe",
+  });
+
+  const paths = res.items.map((item) => {
+    return {
+      params: { slug: item.fields.slug },
+    };
+  });
+
   return {
     paths,
+    // if go to path that does not exist, show 404 page
+    fallback: false,
   };
 }
-export async function getStaticProps() {
+export async function getStaticProps({ params }) {
+  const slug = params.slug;
+
+  const res = await client.getEntries({
+    content_type: "recipe",
+    "fields.slug": slug,
+  });
+  const data = await res.items;
+
+  const recipe = data[0];
+
   return {
-    props,
+    props: {
+      recipe,
+    },
   };
 }
-export default function RecipeDetails() {
-  return <div>Recipe Details</div>;
+export default function RecipeDetails({ recipe }) {
+  console.log(recipe);
+  const { title, slug, cookingTime, thumbnail } = recipe.fields;
+  return (
+    <div>
+      Recipe Details
+      <h1>{title}</h1>
+      <p>{cookingTime}</p>
+    </div>
+  );
 }
